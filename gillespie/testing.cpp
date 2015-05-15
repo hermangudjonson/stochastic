4174abd2da7c19446643e0619e369d41c1ebaa3a
Herman Gudjonson <gyftdresaw@gmail.com>
2015-05-15 11:05:34 -0500

    Makefile testing

--- gillespie/testing.cpp
+++ gillespie/testing.cpp
@@ -15,8 +15,8 @@ int main(int argc, char const ** argv) {
   std::vector<double> times {0.0,0.5,1.0,1.5,2.0,2.5};
   for (int i = 0; i < 3; ++i){
     BasicGillespie BG(IC,times);
-    BG.add_reaction(auto_x);
-    BG.add_reaction(auto_y);
+    BG.add_reaction(std::make_shared<Reaction>(auto_x));
+    BG.add_reaction(std::make_shared<Reaction>(auto_y));
     BG.add_pause(0.5);
     BG.run(3.0,1000);
     BG.print();
